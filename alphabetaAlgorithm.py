b282ee0cc91a13ebfb4fd7eaaa72f097d740e1f7
Jeff Buonamici <jeffbuonamici@users.noreply.github.com>
2021-04-20 17:52:33 -0400

    Final

--- alphabetaAlgorithm.py
+++ alphabetaAlgorithm.py
@@ -3,51 +3,48 @@ import math
 
 
 class alphabeta:
-    def __init__(self,max_depth,max_player):
+    def __init__(self,max_depth):
         self.max_depth_reached = 0
         self.max_depth = max_depth
         self.visited_nodes_count = 0
         self.evaluated_nodes_count = 0
-        self.max_player = max_player 
         self.best_move = None
         self.best_score = 0
 
     def run(self, node, depth, alpha, beta, maximizingPlayer):
-        self.max_depth_reached = max(self.max_depth_reached, depth)
         self.visited_nodes_count += 1
-        if ((self.max_depth_reached == self.max_depth) and not self.max_depth == 0 ) or node.is_terminal():
+
+        if (self.max_depth != 0 and depth == 0 ) or node.is_terminal():
             self.evaluated_nodes_count += 1
-            self.best_score
-            return node.get_score(maximizingPlayer), self.best_move
+            temp = node.get_score(maximizingPlayer)
+            node.score = temp
+            return temp, node
 
         # maxmizingPlayer always wants to return max value
         if maximizingPlayer:
             maxEval = -math.inf
-            bestMove = None
-            for child in node.get_children():
-                eval, oldChild = self.run(child, depth + 1, alpha, beta, False)
-                if eval>self.best_score:
-                    bestMove = child
-                    self.best_move = child
+            for child in node.children:
+                if child.depth > self.max_depth_reached:
+                    self.max_depth_reached = child.depth 
+                eval, move = self.run(child, depth - 1, alpha, beta, False)
+                child.score = eval
                 maxEval = max(maxEval, eval)
                 alpha = max(alpha, eval)
                 if beta <= alpha:
                     break
-            return maxEval, bestMove
+            return maxEval, move
 
         # minimizingPlayer always wants to return min value
         else:
             minEval = math.inf
-            bestMove = None
-            for child in node.get_children():
-                eval, oldChild = self.run(child, depth + 1, alpha, beta, True)
-                if eval< self.best_score:
-                    bestMove = child
-                    self.best_score = eval
-                    self.best_move = child
+            for child in node.children:
+                if child.depth > self.max_depth_reached:
+                    self.max_depth_reached = child.depth 
+                eval, move = self.run(child, depth - 1, alpha, beta, True)
+                child.score = eval
                 minEval = min(minEval, eval)
                 beta = min(beta, eval)
                 if beta <= alpha:
                     break
-            return minEval, bestMove
+            return minEval, move
 
--- app.py
+++ app.py
@@ -4,55 +4,53 @@ import copy
 from alphabetaAlgorithm import alphabeta
 
 class Node:
-    def __init__(self, tokens, lastTokenTaken, parent):
-        self.tokens = tokens
-        self.lastTokenTaken = lastTokenTaken
+    def __init__(self, depth, tokens, token,parent):
         self.parent = parent
-        open("PNT_analysis.txt", "w")
+        self.token = token
+        self.score = None
+        self.depth = depth
+        self.tokens = tokens
+        self.children = self.generate_children()
        
-    def get_children(self, printing = False):
+    def generate_children(self):
         children = list()
-        #print(self.tokens)
-        if self.lastTokenTaken == None:
+        if self.token == None:
             for i in range(len(self.tokens)):
                 if self.tokens[i]<(len(self.tokens)/2) and self.tokens[i] % 2 == 1:
                     new_list = copy.deepcopy(self.tokens)
                     new_list.remove(self.tokens[i])
-                    children.append(Node(new_list, self.tokens[i], self))
+                    children.append(Node(self.depth+1, new_list, self.tokens[i], self))
         else:
             for i in range(len(self.tokens)):
-                if printing:
-                    print ("last: "+str(self.lastTokenTaken)+" , token: "+str(self.tokens[i])+" , multiple: "+str(is_multiple_or_factor(self.lastTokenTaken, self.tokens[i])))
-                if is_multiple_or_factor(self.lastTokenTaken, self.tokens[i]):
+                if self.is_multiple_or_factor(self.token, self.tokens[i]):
                     new_list = copy.deepcopy(self.tokens)
                     new_list.remove(self.tokens[i])
-                    children.append(Node(new_list, self.tokens[i], self))
-                # Return children
+                    children.append(Node(self.depth+1, new_list, self.tokens[i],self))
         return children
 
     def is_terminal(self):
-        return len(self.get_children()) == 0
+        return len(self.children) == 0
 
     def is_prime(self, token):
-        if int(token) < 2:
+        if token < 2:
             return False
-        return all(int(token) % i for i in range(2, int(token)))
+        return all(token % i for i in range(2, token))
 
     def get_score(self, isMaxPlayer):
         if self.is_terminal():
-            score = 1.0
+            score = -1.0
         else:
             if 1 in self.tokens:
                 return 0
     
-            elif self.lastTokenTaken == 1:
-                if len(self.get_children()) % 2 == 1:
+            elif self.token == 1:
+                if len(self.children) % 2 == 1:
                     score = 0.5
                 else:
                     score = -0.5    
 
-            elif self.is_prime(self.lastTokenTaken):
-                if self.count_prime_succession(self.lastTokenTaken)%2 == 1:
+            elif self.is_prime(self.token):
+                if self.count_prime_succession(self.token)%2 == 1:
                     score = 0.7
                 else:
                     score = -0.7
@@ -61,39 +59,41 @@ class Node:
                     score = 0.6
                 else:
                     score = -0.6
-                
+        
+                   
         if isMaxPlayer:
-            return score
+            self.score = score
         else:
-            return score * -1.0
-    
+            self.score = -score
+                    
+        return self.score
+        
     def largest_prime_factor(self):
-        temp = int(self.lastTokenTaken)
+        temp = self.token
         while not temp == 0:
-            if self.is_prime(temp) and self.lastTokenTaken % temp == 0:
+            if self.is_prime(temp) and self.token % temp == 0:
                 break
             temp -= 1
         return temp
 
     def count_prime_succession(self, prime_token):  
-        succession = self.get_children()
+        succession = self.children
         count = 0
         for child in succession:
-            if int(child.lastTokenTaken) % int(prime_token) == 0:
+            if child.token % prime_token == 0:
                 count = count + 1       
         return count
 
-def is_multiple_or_factor(lastTokenTaken, possibleToken):
-    if int(lastTokenTaken) % int(possibleToken) == 0:
-        return True
-    if int(possibleToken) % int(lastTokenTaken) == 0:
-        return True
-    else:
-        return False
-
-def list_diff(list1, list2):
-    return (list(list(set(list1)-set(list2)) + list(set(list2)-set(list1))))
-    
+    def is_multiple_or_factor(self,token, possibleToken):
+        if token == 1 or possibleToken == 1:
+            return True
+        if token % possibleToken == 0:
+            return True
+        if possibleToken % token == 0:
+            return True
+        else:
+            return False
+        
 if __name__ == '__main__':
     # Get input
     num_tokens = int(sys.argv[1])
@@ -104,7 +104,7 @@ if __name__ == '__main__':
     max_depth = int(sys.argv[-1])
 
     # Gets the last token that was taken
-    lastTokenTaken = None if num_taken_tokens == 0 else int(sys.argv[-2])
+    token = None if num_taken_tokens == 0 else int(sys.argv[-2])
 
     # Create list of tokens
     all_tokens = list()
@@ -112,26 +112,23 @@ if __name__ == '__main__':
         all_tokens.append(i)
     
     # Remove tokens that have already been taken
-    all_tokens = list_diff(all_tokens,taken_tokens)
+    all_tokens = (list(list(set(all_tokens)-set(taken_tokens)) + list(set(taken_tokens)-set(all_tokens))))
     
     # Create a node with the input information
-    node = Node(all_tokens, lastTokenTaken, None)
+    root_node = Node(0,all_tokens, token,None)
+    ab = alphabeta(max_depth)
+    score, node = ab.run(root_node, max_depth , -math.inf, math.inf, num_taken_tokens%2==0 )
 
-    # Check if it is first move or not
-    if num_taken_tokens == 0:
-        isFirstMove = True
-    else:
-        isFirstMove = False
-    children = (node.get_children())
-    ab = alphabeta(max_depth,num_taken_tokens%2==0)
-    score, node = ab.run(node, 0 , -math.inf, math.inf, num_taken_tokens%2==1 )
-    a = open("PNT_analysis.txt", "a")
-    a.write("Move: "+ str(ab.best_move.lastTokenTaken))
-    a.write("\nValue: "+score)
+    smallest_move = math.inf
+    for child in root_node.children:
+        if child.score == score and child.token < smallest_move:
+            smallest_move = child.token
+    
+    a = open("PNT_analysis.txt", "w")
+    a.write("\n\ninput: "+str(sys.argv))
+    a.write("\nMove: "+ str(smallest_move))
+    a.write("\nValue: "+str(score))
     a.write("\nNumber of Nodes Visited: " + str(ab.visited_nodes_count))
     a.write("\nNumber of Nodes Evaluated: "+ str(ab.evaluated_nodes_count))
     a.write("\nMax Depth Reached: "+ str(ab.max_depth_reached))
-    a.write("\nAvg Effective Branching Factor: ")
-
-    
-    
+    a.write("\nAvg Effective Branching Factor: " + str(round(((ab.visited_nodes_count-1)/(ab.visited_nodes_count-ab.evaluated_nodes_count)), 1))+"\n")
